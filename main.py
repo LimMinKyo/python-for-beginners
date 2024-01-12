1a0ee72a93a4fb0992a2e8045f00fd4de00b11e7
LimMinKyo <story1400@naver.com>
2024-01-12 10:46:27 +0900

    #3.1 Else & Elif

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-a = 10
+winner = 10
 
-if a == 10:
-    print("True!")
+if winner > 10:
+    print("Winner is greater than 10.")
+elif winner < 10:
+    print("Winner is less than 10.")
+else:
+    print("Winner is 10.")
